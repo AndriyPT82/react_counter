2ae4e083df1f448588ef3fd83eb49aff8389d79a
Andriy Ivanyk <andriy@unknownf02f74bc83f7.lan>
2021-05-16 19:57:35 +0100

    Updates

---
+++ precache-manifest.f78bbd28e90c575f4c209d55fc2bd772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f1b3cdb59a20cfa501c4091626fa11",
+    "url": "./index.html"
+  },
+  {
+    "revision": "726430a15ed32d4ca4c0",
+    "url": "./static/css/main.29fc5556.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726430a15ed32d4ca4c0",
+    "url": "./static/js/main.352a8992.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
